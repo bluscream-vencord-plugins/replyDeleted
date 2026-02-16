4f467f23775a15298a38fb37ffb4fc373f01ebac
Bluscream <Bluscream@users.noreply.github.com>
2026-02-16 02:53:31 +0100

    Refactor: Adhered to Gold Standard conventions

--- index.tsx
+++ index.tsx
@@ -8,18 +8,8 @@ import definePlugin from "@utils/types";
 import { settings } from "./settings";
 // endregion Imports
 
-// region PluginInfo
-export const pluginInfo = {
-    id: "replyDeleted",
-    name: "ReplyDeleted",
-    description: "Intercepts replies to deleted messages and sends them as quoted text",
-    color: "#7289da",
-    authors: [
-        { name: "Bluscream", id: 467777925790564352n },
-        { name: "Assistant", id: 0n }
-    ],
-};
-// endregion PluginInfo
+import { pluginInfo } from "./info";
+export { pluginInfo };
 
 // region Variables
 const logger = new Logger(pluginInfo.id, pluginInfo.color);
---
+++ info.ts
@@ -0,0 +1,10 @@
+export const pluginInfo = {
+    id: "replyDeleted",
+    name: "ReplyDeleted",
+    description: "Intercepts replies to deleted messages and sends them as quoted text",
+    color: "#7289da",
+    authors: [
+        { name: "Bluscream", id: 467777925790564352n },
+        { name: "Assistant", id: 0n }
+    ],
+};
